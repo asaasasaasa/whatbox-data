846f2ae41e99f2ad91a5cbb06ff8ccdaedde5be4
Anthony Ryan <anthonyryan1@gmail.com>
2018-01-05 14:10:21 -0500

    Modify the download strategy to avoid interrupting in-flight XHR/fetch requests

--- init.js
+++ init.js
@@ -1,60 +1,76 @@
+/* jshint strict: true */
+/* globals plugin, theWebUI, theContextMenu */
 (function(){
-	"use strict";
+    "use strict";
 
-	plugin.loadLang();
+	// While in the context menu we can simply use
+	// a a[href][download] to process file downloads,
+	// the double-click process is more complex. So it's
+	// become necessary to use an iframe hack to start
+	// downloads this way
+	var DOWNLOAD_FRAME = document.createElement('iframe');
+	document.body.appendChild(DOWNLOAD_FRAME);
 
-	plugin.config = theWebUI.config;
-	theWebUI.config = function(data)
-	{
-		plugin.config.call(this,data);
-		var oldDblClick = this.getTable("fls").ondblclick;
-		this.getTable("fls").ondblclick = function(obj)
-		{
-			if (plugin.enabled && (theWebUI.dID!="") && (theWebUI.dID.length==40))
-			{
-				if (theWebUI.settings["webui.fls.view"]) {
-					var file_number = obj.id.split('_f_')[1];
-					theWebUI.getData(theWebUI.dID, file_number);
-					return(false);
-				} else {
-					var lnk = this.getAttr(obj.id, "link");
-					if (lnk==null) {
-						theWebUI.getData(theWebUI.dID, obj.id.substr(3));
-						return(false);
-					}
-				}
-			}
-			return(oldDblClick.call(this,obj));
-		}
-	};
+    plugin.loadLang();
 
-	theWebUI.getData = function( hash, no ) {
-		window.location = './plugins/whatbox-data/action.php?hash=' + hash + '&no=' + no;
-	};
+    plugin.config = theWebUI.config;
+    theWebUI.config = function(data)
+    {
+        plugin.config.call(this,data);
+        var oldDblClick = this.getTable("fls").ondblclick;
+        this.getTable("fls").ondblclick = function(obj)
+        {
+            if (plugin.enabled && (theWebUI.dID!="") && (theWebUI.dID.length==40))
+            {
+                if (theWebUI.settings["webui.fls.view"]) {
+                    var file_number = obj.id.split('_f_')[1];
+					DOWNLOAD_FRAME.src = './plugins/whatbox-data/action.php?hash=' + theWebUI.dID + '&no=' + file_number;
+                    return(false);
+                } else {
+                    var lnk = this.getAttr(obj.id, "link");
+                    if (lnk==null) {
+						DOWNLOAD_FRAME.src = './plugins/whatbox-data/action.php?hash=' + theWebUI.dID + '&no=' + obj.id.substr(3);
+                        return(false);
+                    }
+                }
+            }
+            return(oldDblClick.call(this,obj));
+        };
+    };
 
-	if (plugin.canChangeMenu()) {
-		plugin.createFileMenu = theWebUI.createFileMenu;
-		theWebUI.createFileMenu = function( e, id ) {
-			if (plugin.createFileMenu.call(this, e, id)) {
-				if (plugin.enabled) {
-					theContextMenu.add([CMENU_SEP]);
-					var fno = null;
-					var table = this.getTable("fls");
-					if (table.selCount != 1) {
-						return false;
-					}
+    theWebUI.getData = function( hash, no ) {
+        DOWNLOAD_FRAME.src = './plugins/whatbox-data/action.php?hash=' + hash + '&no=' + no;
+    };
 
-					var fid = table.getFirstSelected();
-					if (this.settings["webui.fls.view"]) {
-						fno = fid.split('_f_')[1];
-					} else if (!this.dirs[this.dID].isDirectory(fid)) {
-						fno = fid.substr(3);
-					}
-					theContextMenu.add( [theUILang.getData, "theWebUI.getData('" + theWebUI.dID + "',"+fno+")"] );
-				}
-				return(true);
-			}
-			return(false);
-		}
-	}
+    if (plugin.canChangeMenu()) {
+        plugin.createFileMenu = theWebUI.createFileMenu;
+        theWebUI.createFileMenu = function( e, id ) {
+            if (plugin.createFileMenu.call(this, e, id)) {
+                if (plugin.enabled) {
+                    theContextMenu.add([CMENU_SEP]);
+                    var fno = null;
+                    var table = this.getTable("fls");
+                    if (table.selCount != 1) {
+                        return false;
+                    }
+
+                    var fid = table.getFirstSelected();
+                    if (this.settings["webui.fls.view"]) {
+                        fno = fid.split('_f_')[1];
+                    } else if (!this.dirs[this.dID].isDirectory(fid)) {
+                        fno = fid.substr(3);
+                    }
+                    theContextMenu.add([
+                        theUILang.getData,
+                        function() {
+                            this.href = './plugins/whatbox-data/action.php?hash=' + theWebUI.dID + '&no=' + fno;
+                            this.setAttribute('download', '');
+                        }
+                    ]);
+                }
+                return(true);
+            }
+            return(false);
+        };
+    }
 })();
